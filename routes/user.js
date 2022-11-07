6587013ef377384ee9f6fcf9b4f08e7fee6dcd59
jakariaweb <jakariahossainweb@gmail.com>
2022-11-07 16:05:55 +0600

    Updated

--- routes/user.js
+++ routes/user.js
@@ -2,8 +2,11 @@ const express = require('express')
 
 const router = express.Router()
 
+router.get('/', (req, res) => {
+  res.send('Backend working....')
+})
 router.get('/signup', (req, res) => {
-  res.send('Working....')
+  res.send('Signup Page')
 })
 
 module.exports = router
